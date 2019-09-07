ad31923125a028c5d8e8fb5b77b2113897fe6c61
childsluke <54657692+childsluke@users.noreply.github.com>
2019-09-07 11:43:08 -0700

    Add files via upload
    Added in block rotation & collision detection on all on-screen blocks. Still have TODO: Tetris 'bag' randomnisaton algorithm, line clearing, fix x-axis intersection collision detection bug.

--- tetrisgame.py
+++ tetrisgame.py
@@ -1,12 +1,12 @@
 # A TETRIS CLONE UTILIZING PYGAME WRITTEN AS A FIRST 'SIMPLE' GAME PROJECT BY LUKE CHILDS (STARTED 9/5/2019)
 #------------------------------------------------------------------------------------------------------------
 #
-# So far, a random tetris block will auto-generate which you can move. It will then move toward the bottom of the screen, and auto-generate
-# a new block when it reaches the bottom. You can move left or right with the arrow keys, and increase downward velocity with the down arrow.
+# Left/Right arrows to move block. Down arrows to 'drop' block. Space bar to rotate block.
+#
 # ESCAPE quits.
 # Movement speed has now been synced via an in-game clock
 #
-# TODO: Tetris 'bag' randomnisation algorithm, Collision Detection with other blocks, Active block Rotation, Line Clearing
+# TODO: Line Clearing, Tetris 'bag' randomnisation algorithm, fix x axis intersection with collision detection
 # Possible fun bonuses: Main Menu & Game Over screens, Allow player to vary speed & block size, introduce color variation & matching for bonus points
 
 #  COPYRIGHT 2019 - Luke Childs
@@ -41,9 +41,6 @@ class Block:
         return self.boundingRectangle.colliderect(incomingBlock.boundingRectangle)
 
     def updateBlock(self):
-        if(self.position.y >= 640 - blockSize):
-            self.downwardVelocity = 0
-            self.position.y = 640 - blockSize
         self.boundingRectangle = pygame.Rect(self.position.x, self.position.y, blockSize, blockSize)
 
 class TetrisBlock:
@@ -150,6 +147,7 @@ def main():
     pygame.display.set_caption("Tetris Demo")
     gameSurface = pygame.display.set_mode((640, 640))
     gameClock = pygame.time.Clock()
+    
     # TODO: Main Menu screen
     
     # Here's where we start the game - should be its own
@@ -158,14 +156,22 @@ def main():
     activeTetrisBlock = 0
     nextRandomBlockNumber = random.randint(1,7)
     currentRandomBlockNumber = nextRandomBlockNumber
+    leftOrRightPressed = True
+    downPressed = False
     
     nextRandomBlock = TetrisBlock(nextRandomBlockNumber)
     nextRandomBlock.downwardVelocity = 0
+    spaceAlreadyPressed = False
+    rotationActive = False
     
     score = 0
     gameRunning = True
     allBlocksStopped = True
     velocityFactor = defaultVelocityFactor
+    gameFont = pygame.font.SysFont("Arial", 22)
+    scoreTitleText = gameFont.render("SCORE", True, (255,255,255)) 
+    scoreText = gameFont.render(str(score), True, (255,255,255))
+    nextText = gameFont.render("NEXT", True, (255,255,255))
 
     # Play the now infamous in-game music!
     pygame.mixer.music.load("Tetris.mp3")
@@ -175,14 +181,27 @@ def main():
     #blocksOnScreen.append( Block(0,0,(0,0,0)) )  
 
     while gameRunning:
-    
+
         pressed = pygame.key.get_pressed()
         
+        # UI rendering
+        gameSurface.blit(scoreTitleText, (545, 500))
+        gameSurface.blit(scoreText, (570, 550))
+        gameSurface.blit(nextText, (550, 150))
+        
+        pygame.draw.line(gameSurface, (255,255,255), (500, 0), (500,640), 2)
+        
+        # Game rendering
         # Draw the next random block to appear
-        for blockToDraw in nextRandomBlock.blocks:
-            pygame.draw.rect(gameSurface, blockToDraw.blockColor, pygame.Rect(blockToDraw.position.x + 225, blockToDraw.position.y + 50, blockSize, blockSize ))
-            pygame.draw.rect(gameSurface, (255,255,255), pygame.Rect(blockToDraw.position.x + 225, blockToDraw.position.y + 50, blockSize, blockSize ), 2)
-           
+        if(not nextRandomBlockNumber == 1):
+            for blockToDraw in nextRandomBlock.blocks:
+                pygame.draw.rect(gameSurface, blockToDraw.blockColor, pygame.Rect(blockToDraw.position.x + 250, blockToDraw.position.y + 50, blockSize, blockSize ))
+                pygame.draw.rect(gameSurface, (255,255,255), pygame.Rect(blockToDraw.position.x + 250, blockToDraw.position.y + 50, blockSize, blockSize ), 2)
+        else:
+            for blockToDraw in nextRandomBlock.blocks:
+                pygame.draw.rect(gameSurface, blockToDraw.blockColor, pygame.Rect(blockToDraw.position.x + 225, blockToDraw.position.y + 50, blockSize, blockSize ))
+                pygame.draw.rect(gameSurface, (255,255,255), pygame.Rect(blockToDraw.position.x + 225, blockToDraw.position.y + 50, blockSize, blockSize ), 2) 
+        
         # Draw and update bounding box placement for all on-screen blocks in play in their position
         for blockToDraw in blocksOnScreen:
             blockToDraw.updateBlock()
@@ -227,9 +246,22 @@ def main():
             # Move the active block down the screen
             blocksOnScreen[currentIteration].moveDown(velocityFactor)
             
+            # Stop if we've reached the bottom!
+            if(blocksOnScreen[currentIteration].position.y >= 640 - blockSize):
+                blocksOnScreen[currentIteration].downwardVelocity = 0
+                blocksOnScreen[currentIteration].position.y = 640 - blockSize
+            
             # Check for left or right movement (if block is not already against the wall!)
             activeBlockAgainstLeftWall = False
             activeBlockAgainstRightWall = False
+            
+            # TODO: Do rotation if we've pressed Space
+            if rotationActive:
+                pivotX = blocksOnScreen[len(blocksOnScreen) - 1].position.x
+                pivotY = blocksOnScreen[len(blocksOnScreen) - 1].position.y
+                
+                blocksOnScreen[currentIteration].position = (blocksOnScreen[currentIteration].position - pygame.Vector2(pivotX, pivotY)).rotate(90)
+                blocksOnScreen[currentIteration].position += pygame.Vector2(pivotX, pivotY)
             
             wallTestCounter = 0
             currentWallTestIteration = 0
@@ -238,9 +270,11 @@ def main():
                 currentWallTestIteration = len(blocksOnScreen) - wallTestCounter
                 if blocksOnScreen[currentWallTestIteration].position.x <= 0:
                     activeBlockAgainstLeftWall = True
+                    blocksOnScreen[currentIteration].position.x += leftRightMovementFactor * 2
                     break
-                if blocksOnScreen[currentWallTestIteration].position.x >= 640 - blockSize:
+                if blocksOnScreen[currentWallTestIteration].position.x >= 500 - blockSize:
                    activeBlockAgainstRightWall = True
+                   blocksOnScreen[currentIteration].position.x -= leftRightMovementFactor * 2
                      
 
             if pressed[pygame.K_LEFT] and activeBlockAgainstLeftWall == False:
@@ -249,19 +283,30 @@ def main():
                     blocksOnScreen[currentIteration].moveLeftorRight(leftRightMovementFactor)
          
             # TODO: Space bar rotates active block (somehow...lol)
-            # TODO: Do collision tests (ie stop entire active Tetris block if one of its blocks collides with one other block) 
+            # TODO: Do collision tests (ie stop entire active Tetris block if one of its blocks collides with one other block)
+            collisionTestCounter = 0
+            while(collisionTestCounter < len(blocksOnScreen) - 4):
+                if blocksOnScreen[currentIteration].collisionTest(blocksOnScreen[collisionTestCounter]):
+                    collisionApplyCounter = 1
+                    while collisionApplyCounter <= 4:
+                        blocksOnScreen[len(blocksOnScreen) - collisionApplyCounter].downwardVelocity = 0
+                        blocksOnScreen[len(blocksOnScreen) - collisionApplyCounter].position.y -= velocityFactor
+                        collisionApplyCounter += 1
+                    allBlocksStopped = True
+                    break
+                collisionTestCounter += 1
     
-            # Game over if active block intersects with top of screen
-            if blocksOnScreen[currentIteration].position.y <= -5:
+            # Game over if last block placed intersects with top of screen
+            if blocksOnScreen[len(blocksOnScreen) - 5].position.y <= blockSize:
                 gameRunning = False
                 break
 
         # Handle keyboard down arrow (increase speed & 'drop' block)
-        if pressed[pygame.K_DOWN] and velocityFactor < 2.5:
-            velocityFactor += defaultVelocityFactor
-            if(velocityFactor > 2.5):
-                velocityFactor = 2.5
-                
+        if pressed[pygame.K_DOWN] and velocityFactor < 2.5 and not downPressed:
+            velocityFactor = 2.5
+            downPressed = True       
+        
+        rotationActive = False
         # ESCAPE quits
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -270,7 +315,16 @@ def main():
                 if event.type == pygame.KEYDOWN:
                     if(event.key ==  pygame.K_ESCAPE):
                         gameRunning = False
-                        
+                    # TODO: Space bar rotates active Tetris block 90 degrees
+                    if event.key == pygame.K_SPACE and not spaceAlreadyPressed:
+                        spaceAlreadyPressed = True
+                        rotationActive = True        
+                if event.type == pygame.KEYUP:
+                    if(event.key == pygame.K_SPACE):
+                        spaceAlreadyPressed = False
+                    if event.key == pygame.K_DOWN:
+                        downPressed = False
+               
         pygame.display.flip()
         pygame.display.update()
         gameSurface.fill((0,0,0))
